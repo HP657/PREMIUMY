f205bef47ebaeecde01225559a287a08aa5b0b04
HP657 <junlee5070@gmail.com>
2025-07-17 15:44:41 +0900

    ♻️ refactor(core): apply global prefix '/api' for all API routes

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(Logger));
   const PORT = process.env.PORT || 3000;
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '/', method: RequestMethod.GET }],
+  });
   await app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
   });
