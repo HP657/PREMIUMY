a3eb50db990d1d4e31776e426a8a9ef3f8de88c2
HP657 <junlee5070@gmail.com>
2025-07-20 18:22:55 +0900

    🔒 security(auth): apply @Public() decorator to login API

--- BackEnd/src/auth/auth.controller.ts
+++ BackEnd/src/auth/auth.controller.ts
@@ -2,16 +2,19 @@ import { Controller, Get, UseGuards, Req, Res, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get('google')
+  @Public()
   @UseGuards(AuthGuard('google'))
   async googleLogin(): Promise<void> {}
 
   @Get('google/redirect')
+  @Public()
   @UseGuards(AuthGuard('google'))
   async googleLoginCallback(@Req() req, @Res() res: Response): Promise<void> {
     const jwt: string = req.user.jwt;
@@ -28,10 +31,12 @@ export class AuthController {
   }
 
   @Get('github')
+  @Public()
   @UseGuards(AuthGuard('github'))
   async githubLogin(): Promise<void> {}
 
   @Get('github/redirect')
+  @Public()
   @UseGuards(AuthGuard('github'))
   async githubLoginCallback(@Req() req, @Res() res: Response): Promise<void> {
     const jwt: string = req.user.jwt;
@@ -48,10 +53,12 @@ export class AuthController {
   }
 
   @Get('kakao')
+  @Public()
   @UseGuards(AuthGuard('kakao'))
   async kakaoLogin(): Promise<void> {}
 
   @Get('kakao/redirect')
+  @Public()
   @UseGuards(AuthGuard('kakao'))
   async kakaoLoginCallback(@Req() req, @Res() res: Response): Promise<void> {
     const jwt: string = req.user.jwt;
@@ -68,10 +75,12 @@ export class AuthController {
   }
 
   @Get('naver')
+  @Public()
   @UseGuards(AuthGuard('naver'))
   async naverLogin(): Promise<void> {}
 
   @Get('naver/redirect')
+  @Public()
   @UseGuards(AuthGuard('naver'))
   async naverLoginCallback(@Req() req, @Res() res: Response): Promise<void> {
     const jwt: string = req.user.jwt;
