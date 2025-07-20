c195589b96f4eb546352970e26cecd13d38eba9d
HP657 <junlee5070@gmail.com>
2025-07-20 11:53:24 +0900

    🔧 chore(app): apply cookie-parser and configure CORS for frontend integration

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -2,14 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
 import { RequestMethod } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(Logger));
+
   const PORT = process.env.PORT || 3000;
+
   app.setGlobalPrefix('api', {
     exclude: [{ path: '/', method: RequestMethod.GET }],
   });
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   await app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
   });
